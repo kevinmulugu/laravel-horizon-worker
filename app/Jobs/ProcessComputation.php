2562bc7c8ab072b49b0b7c19f0eadd1146241448
Kevin Mulugu <kevinmulugu@gmail.com>
2022-05-18 11:16:44 +0300

    worker initialization

---
+++ app/Jobs/ProcessComputation.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessComputation implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $data;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param $data
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $sum = $this->data['num1'] + $this->data['num2'];
+
+        app('log')->info('Sum of '.$this->data['num1'] . ' and '.$this->data['num1'] . ' is ' .$sum);
+    }
+}
